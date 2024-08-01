c31c6f4a5e0fa6cb37937def5523072411647dd3
Devi Charan <145871325+cherry-1729-9090@users.noreply.github.com>
2024-08-01 20:26:37 +0530

    changed the backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
-require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
-const app = require('./src/app');
+const dataRoutes = require('./src/Routes/routes');
 const dataFetcher = require('./src/utils/fetchData');
 
-const port = process.env.PORT || 3000;
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
+
+app.use('/api', dataRoutes);
 
 dataFetcher.fetchAndStoreData()
   .then(() => {
